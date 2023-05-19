0b494cb398c1cb0c2986d6a6c9b844bae9dceb9b
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2023-05-19 19:26:08 +0200

    Removed image test from totalcount

--- pages/api/totalcount.ts
+++ pages/api/totalcount.ts
@@ -1,8 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import fs from 'fs'
-import path from 'path'
-const sharp = require('sharp')
 const prisma = new PrismaClient()
 
 type ObjCountRouteOutput = {
@@ -22,27 +19,16 @@ async function getObjCount() {
     return { items_count, blocks_count, advancements_count, tags_count, recipes_count }
 }
 
-const filePath = path.resolve('.', 'acacia_boat.png')
-
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-    // getObjCount().then((count) => {
-    //     let objCount: ObjCountRouteOutput = {
-    //         items_count: count.items_count,
-    //         blocks_count: count.blocks_count,
-    //         advancements_count: count.advancements_count,
-    //         tags_count: count.tags_count,
-    //         recipes_count: count.recipes_count
-    //     }
-    //     res.status(200).json(objCount);
-    // });
-    //resize image
-    let width: number = parseInt(req.query.w as string);
-    let height: number = parseInt(req.query.h as string);
-    const imageBuffer = await sharp(filePath)
-        .resize(width, height)
-    res.setHeader('Content-Type', 'image/png')
-    res.send(imageBuffer)
-
+    let count = await getObjCount();
+    let objCount: ObjCountRouteOutput = {
+        items_count: count.items_count,
+        blocks_count: count.blocks_count,
+        advancements_count: count.advancements_count,
+        tags_count: count.tags_count,
+        recipes_count: count.recipes_count
+    }
+    res.status(200).json(objCount);
     prisma.$disconnect();
 
 }
