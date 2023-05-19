d93a0c680e48f6fbb9c7addf4177a956728e779b
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2023-05-19 19:25:53 +0200

    New filters for tags

--- pages/api/tags.ts
+++ pages/api/tags.ts
@@ -7,20 +7,22 @@ const prisma = new PrismaClient()
 type TagsRouteProps = {
 	tag_id?: string
 	name?: string
+	father_tag?: string
 }
 
 async function getTags(query: TagsRouteProps) {
 	const tags = await prisma.tags.findMany({
 		where: {
 			tag_id: query.tag_id,
-			name: query.name
+			name: query.name,
+			father_tag: query.father_tag
 		}
 	});
 	return tags
 }
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-    getTags(req.query).then((tags) => {
-        res.status(200).json(tags);
-    });
+    let tags = await getTags(req.query);
     prisma.$disconnect();
-}   
+    res.status(200).json(tags);
+    res.end();
+}
